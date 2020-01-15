c6bcd2ac3ad6588f295b2ed51d9aba2f4c56cb29
Tomasz TJ Janczewski <tomasz@3pio.pl>
2020-01-15 17:47:33 +0100

    RCE check

--- rce_check.sh
+++ rce_check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "RCE check"
-var1 = $(grep -r --include=*.{cc,h,java,php,c} "Runtime.getRuntime().exec(" .)
+var1=$(grep -r --include=*.{cc,h,java,php,c} "Runtime.getRuntime().exec(" .)
 echo $var1
 if [ -z "$var1" ]
 then
