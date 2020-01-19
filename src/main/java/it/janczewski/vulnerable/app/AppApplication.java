0fe80f1f62bc6cfc4873f77e8831ecef734cf991
Tomasz TJ Janczewski <tomasz@3pio.pl>
2020-01-19 13:55:35 +0100

    RCE check live test

--- src/main/java/it/janczewski/vulnerable/app/AppApplication.java
+++ src/main/java/it/janczewski/vulnerable/app/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
-//Runtime.getRuntime().exec(
+//  Runtime.getRuntime().exec(
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
